50253c783d41a859ad3e8dfca81a95f18a0bb992
Jonathan Kingston <jonathan@jooped.co.uk>
2015-05-12 23:33:33 +0100

    Adding in Ember exceptions and updating tests to build output checking

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+var assign = require('object-assign');
 var config = require('eslint-config-nightmare-mode');
+// Ember specific overrides
+var overrides = {
+ "rules": {
+   "new-cap": [0,
+      {
+        "capIsNewExceptions": [
+            "Array",
+            "Boolean",
+            "Date",
+            "Error",
+            "Function",
+            "Number",
+            "Object",
+            "RegExp",
+            "String",
+            "Symbol",
+  
+            "Ember",
+            "A"
+        ]
+      }
+    ]
+  }
+};
+
+assign(config.rules, overrides.rules);
 
 module.exports = config;
